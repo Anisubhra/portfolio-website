d3b16fefdb7022232b90fbe2282dc275d2556ba3
Anisubhra <anisubhrasarkar@gmail.com>
2024-02-12 19:09:15 +0530

    changes to content

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,10 +62,10 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Anisubhra.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Anisubhra.</span><br /> I'm a{" "}
         <span className="font-bold">Software Developer</span> with{" "}
         <span className="font-bold">5 years</span> of experience. I enjoy
-        building visually appealing and cross platform <span className="italic">websites & apps</span>. My primary focus is in {" "}
+        building visually appealing and cross platform <span className="italic">websites & apps</span>. My primary focus is in {" "}<br />
         <span className="underline">Front-end (React, Next.js, Angular)</span>.
       </motion.h1>
 
